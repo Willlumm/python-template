097a20874f5d0277bb89ec4d800f591ec6363b12
William Lumme <william.lumme@gmail.com>
2024-11-13 23:06:07 +0000

    Revert "feat: add comment (#26)"
    This reverts commit c4d14dc068a507e5f65f260f7a774796e5423b16.

--- mypkg/a.py
+++ mypkg/a.py
@@ -2,7 +2,6 @@ from datetime import UTC, date, datetime
 
 
 def hello(birthday: date | None = None) -> str:
-    # comment!
     if datetime.now(tz=UTC).date() == birthday:
         return "Happy Birthday from `a.py`!"
     return "Hello from `a.py`!"
