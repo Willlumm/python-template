c1f97ab28942a710cabaae84ac7a05070f0e958f
William Lumme <80401792+Willlumm@users.noreply.github.com>
2024-11-13 22:58:08 +0000

    fix: typo

--- mypkg/a.py
+++ mypkg/a.py
@@ -4,5 +4,5 @@ from datetime import UTC, date, datetime
 def hello(birthday: date | None = None) -> str:
     # comment!
     if datetime.now(tz=UTC).date() == birthday:
-        return "Happy Birthday to from `a.py`!"
+        return "Happy Birthday from `a.py`!"
     return "Hello from `a.py`!"
